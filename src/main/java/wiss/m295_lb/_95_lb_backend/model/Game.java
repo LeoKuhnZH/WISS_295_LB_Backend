59a7a87c725b8b1c2513aa6aff6a23ef5de0366b
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-12 12:01:14 +0100

    made the models

---
+++ src/main/java/wiss/m295_lb/_95_lb_backend/model/Game.java
@@ -0,0 +1,8 @@
+package wiss.m295_lb._95_lb_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="game")
+public class Game {
+}
---
+++ src/main/java/wiss/m295_lb/_95_lb_backend/model/Genre.java
@@ -0,0 +1,8 @@
+package wiss.m295_lb._95_lb_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="genre")
+public class Genre {
+}
