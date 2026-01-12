08d94d8e767195058c91b89d7f8a3e75335eba7b
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-12 14:57:24 +0100

    made the repositories for the connection tables

---
+++ src/main/java/wiss/m295_lb/_95_lb_backend/repository/GameGenreRepository.java
@@ -0,0 +1,7 @@
+package wiss.m295_lb._95_lb_backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import wiss.m295_lb._95_lb_backend.model.GameGenre;
+
+public interface GameGenreRepository extends CrudRepository<GameGenre, Integer> {
+}
---
+++ src/main/java/wiss/m295_lb/_95_lb_backend/repository/UserGameRepository.java
@@ -0,0 +1,7 @@
+package wiss.m295_lb._95_lb_backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import wiss.m295_lb._95_lb_backend.model.UserGame;
+
+public interface UserGameRepository extends CrudRepository<UserGame, Integer> {
+}
