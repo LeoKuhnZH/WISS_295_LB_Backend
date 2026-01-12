339c197d947a660d3999f6aee0ba36ae1bbe85c5
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-12 14:02:49 +0100

    made getters and setters for Genre.java

--- src/main/java/wiss/m295_lb/_95_lb_backend/model/Genre.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/model/Genre.java
@@ -20,4 +20,28 @@ public class Genre {
 
     @Column(name="description", nullable = true)
     private Optional<String> description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description.orElse("");
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : Optional.of(description);
+    }
 }
