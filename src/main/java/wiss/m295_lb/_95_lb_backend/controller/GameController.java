d7c6c017458afcfb65e07d9b6abe7f29b00d3f26
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-19 11:54:10 +0100

    started working on the controllers

--- src/main/java/wiss/m295_lb/_95_lb_backend/controller/GameController.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/controller/GameController.java
@@ -1,4 +1,13 @@
 package wiss.m295_lb._95_lb_backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import wiss.m295_lb._95_lb_backend.repository.GameRepository;
+
+@RestController
+@RequestMapping("/api/game")
 public class GameController {
+
+    @Autowired
+    private GameRepository gameRepository;
 }
--- src/main/java/wiss/m295_lb/_95_lb_backend/controller/GenreController.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/controller/GenreController.java
@@ -1,4 +1,13 @@
 package wiss.m295_lb._95_lb_backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import wiss.m295_lb._95_lb_backend.repository.GenreRepository;
+
+@RestController
+@RequestMapping("/api/genre")
 public class GenreController {
+
+    @Autowired
+    private GenreRepository genreRepository;
 }
