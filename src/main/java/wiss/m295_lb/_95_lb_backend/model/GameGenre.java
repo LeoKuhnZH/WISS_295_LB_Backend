d898f4e0afc5878c4819fd9daf5265c0b1d241f2
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-19 09:17:04 +0100

    removed the redundant GameGenre object

--- src/main/java/wiss/m295_lb/_95_lb_backend/model/GameGenre.java
+++
@@ -1,7 +0,0 @@
-package wiss.m295_lb._95_lb_backend.model;
-
-/**
- * Connection table between Game and Genre
- */
-public class GameGenre {
-}
--- src/main/java/wiss/m295_lb/_95_lb_backend/repository/GameGenreRepository.java
+++
@@ -1,7 +0,0 @@
-package wiss.m295_lb._95_lb_backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import wiss.m295_lb._95_lb_backend.model.GameGenre;
-
-public interface GameGenreRepository extends CrudRepository<GameGenre, Integer> {
-}
