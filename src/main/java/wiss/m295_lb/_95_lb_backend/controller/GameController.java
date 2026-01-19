eda34b41aaf77871a62c071c0b6b15c232df9c85
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-19 12:45:18 +0100

    decided to use @Valid instead of @Validated

--- src/main/java/wiss/m295_lb/_95_lb_backend/controller/GameController.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/controller/GameController.java
@@ -1,9 +1,9 @@
 package wiss.m295_lb._95_lb_backend.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import wiss.m295_lb._95_lb_backend.model.Game;
 import wiss.m295_lb._95_lb_backend.repository.GameRepository;
@@ -12,9 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 
-/**
- * The type Game controller.
- */
 @RestController
 @RequestMapping("/api/game")
 public class GameController {
@@ -22,66 +19,37 @@ public class GameController {
     @Autowired
     private GameRepository gameRepository;
 
-    /**
-     * Instantiates a new Game controller.
-     *
-     * @param gameRepository the game repository
-     */
     public GameController(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
 
-    /**
-     * Gets game.
-     *
-     * @param id the id
-     * @return the game
-     */
     @GetMapping("/{id}")
     public ResponseEntity<Game> getGame(@PathVariable Long id) {
         Optional<Game> game = gameRepository.findById(id);
         return game.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    /**
-     * Gets all games.
-     *
-     * @return the all games
-     */
     @GetMapping
     public ResponseEntity<List<Game>> getAllGames() {
         return ResponseEntity.ok(gameRepository.findAll());
     }
 
-    /**
-     * Create game response entity.
-     *
-     * @param game the game
-     * @return the response entity
-     */
     @PostMapping
     public ResponseEntity<Game> createGame(@RequestBody Game game) {
         Game createdGame = gameRepository.save(game);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdGame);
     }
 
-    /**
-     * Update game response entity.
-     *
-     * @param id   the id
-     * @param game the game
-     * @return the response entity
-     */
     @PutMapping("/{id}")
     public ResponseEntity<Game> updateGame(
             @PathVariable Long id,
-            @Validated @RequestBody Game game
+            @Valid @RequestBody Game game
     ) {
         Optional<Game> existingGame = gameRepository.findById(id);
         if (existingGame.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        
+
         Game gameToUpdate = existingGame.get();
         gameToUpdate.setTitle(game.getTitle());
         if (game.getDescription().isPresent()) {
@@ -91,17 +59,11 @@ public class GameController {
         }
         gameToUpdate.setRating(game.getRating());
         gameToUpdate.setGenres(game.getGenres());
-        
+
         Game updatedGame = gameRepository.save(gameToUpdate);
         return ResponseEntity.ok(updatedGame);
     }
 
-    /**
-     * Delete game response entity.
-     *
-     * @param id the id
-     * @return the response entity
-     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Game> deleteGame(@PathVariable Long id) {
         Optional<Game> game = gameRepository.findById(id);
