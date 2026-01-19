d67c323b5e908d9738e744ef3a78577007c78c8c
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-19 14:09:48 +0100

    added some autogenerated Javadocs

--- src/main/java/wiss/m295_lb/_95_lb_backend/model/Genre.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/model/Genre.java
@@ -25,34 +25,74 @@ public class Genre {
     @ManyToMany(mappedBy = "genres")
     private Set<Game> games;
 
+    /**
+     * Gets genre id.
+     *
+     * @return the genre id
+     */
     public Long getGenreId() {
         return genreId;
     }
 
+    /**
+     * Sets genre id.
+     *
+     * @param genreId the genre id
+     */
     public void setGenreId(Long genreId) {
         this.genreId = genreId;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets games.
+     *
+     * @return the games
+     */
     public Set<Game> getGames() {
         return games;
     }
 
+    /**
+     * Sets games.
+     *
+     * @param games the games
+     */
     public void setGames(Set<Game> games) {
         this.games = games;
     }
