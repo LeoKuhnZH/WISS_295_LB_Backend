c985541cf7c72d8c81bc42d84c9fb344161b1266
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-01-19 09:18:34 +0100

    made my ids to long

--- src/main/java/wiss/m295_lb/_95_lb_backend/model/User.java
+++ src/main/java/wiss/m295_lb/_95_lb_backend/model/User.java
@@ -11,16 +11,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
-    private Integer id;
+    private Long id;
 
     @Column(name = "username", nullable = false)
     private String name;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
