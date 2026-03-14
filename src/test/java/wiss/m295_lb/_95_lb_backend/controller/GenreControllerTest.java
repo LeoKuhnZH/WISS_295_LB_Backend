2ba97338b09920dde47e11397dc45f00c62b33ed
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-03-14 15:12:39 +0100

    made a find by id found test

--- src/test/java/wiss/m295_lb/_95_lb_backend/controller/GenreControllerTest.java
+++ src/test/java/wiss/m295_lb/_95_lb_backend/controller/GenreControllerTest.java
@@ -5,22 +5,43 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import wiss.m295_lb._95_lb_backend.model.Genre;
 import wiss.m295_lb._95_lb_backend.repository.GenreRepository;
 
-/**
- * The type Genre controller test.
- */
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class GenreControllerTest {
+    private final static Long  GENRE_ID = 7L;
+
     @InjectMocks
     private GenreController testee;
 
     @Mock
     private GenreRepository genreRepository;
+    @Mock
+    private Genre genre;
+    @Mock
+    private List<Genre> genres;
 
-    /**
-     * Gets genre by id.
-     */
+    @Test
+    void getGenreById_found_genre() {
+        // arrange
+        when(genreRepository.findById(GENRE_ID)).thenReturn(Optional.of(genre));
+        // act
+        ResponseEntity<Genre> actual = testee.getGenreById(GENRE_ID);
+        // assert
+        assertThat(actual.getBody()).isEqualTo(genre);
+    }
+
     @Test
     void getGenreById() {
         // arrange
