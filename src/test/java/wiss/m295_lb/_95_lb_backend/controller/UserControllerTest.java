0ac2fd59f5e94b8515d189efb59f33e165726dbf
Kagemono <shootgunxdhd@gmail.com>
2026-03-20 01:33:55 +0100

    did some user testing setup

--- src/test/java/wiss/m295_lb/_95_lb_backend/controller/UserControllerTest.java
+++ src/test/java/wiss/m295_lb/_95_lb_backend/controller/UserControllerTest.java
@@ -1,11 +1,17 @@
 package wiss.m295_lb._95_lb_backend.controller;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import wiss.m295_lb._95_lb_backend.model.User;
 import wiss.m295_lb._95_lb_backend.repository.UserRepository;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
@@ -14,6 +20,19 @@ class UserControllerTest {
 
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private User user, savedUser, existingUser;
+    @Mock
+    private List<User> users;
+
+    @AfterEach
+    void afterEight() {
+        verifyNoMoreInteractions(
+                userRepository,
+                user,savedUser,existingUser,
+                users
+        );
+    }
 
     @Test
     void findById_found_user() {
